ce51346d785f9636021d264494e14a205635d7c5
Md-Yousuf-sheikh <sheikhyousuf702@gmail.com>
2024-11-28 02:33:35 +0600

    feat: signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,33 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
-    return this.authService.signup();
+  signup(@Body() bto: AuthDto) {
+    return this.authService.signup(bto);
   }
 
   @Post('signin')
-  login() {
+  login(@Req() res: Request) {
+    console.log('res', res);
     // Implement login logic here
     return this.authService.signin();
-  }
-
-  @Post('logout')
-  logout() {
-    // Implement logout logic here
-  }
-
-  @Post('refresh-token')
-  refreshToken() {
-    // Implement token refresh logic here
-  }
-
-  @Post('forgot-password')
-  forgotPassword() {
-    // Implement forgot password logic here
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as argon from 'argon2';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return { msg: 'I have signed up' };
+  async signup(dto: AuthDto) {
+    // generate the password hash
+
+    try {
+      const hash = await argon.hash(dto.password);
+      // save the user db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+      });
+
+      console.log('user', user);
+
+      return { msg: 'I have signed up', data: user };
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Credentials taken');
+      }
+
+      throw error;
+    }
   }
 
   signin() {
